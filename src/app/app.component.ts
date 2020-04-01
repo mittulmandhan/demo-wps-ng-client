a7c038093ab6e29b9613554ffe4520eef4d34354
mittulmandhan <conanmittul@gmail.com>
2020-04-02 03:27:17 +0530

    template and component ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-wps-ng-client';
+  title = 'Demo WPS Angular Client';
+  serviceUrls = [
+    'http://geoprocessing.demo.52north.org:8080/wps/WebProcessingService',
+    'https://riesgos.52north.org/wps/WebProcessingService',
+    'http://geoprocessing.demo.52north.org:8080/javaps/service'
+  ];
+  selectedUrl = '';
+  selectedVersion: '1.0.0' | '2.0.0' = '2.0.0';
+  selectedProcessId = '';
+  response = ``;
+
+  call_getCapabilities() { }
+
+  call_DescribeProcess() { }
 }
