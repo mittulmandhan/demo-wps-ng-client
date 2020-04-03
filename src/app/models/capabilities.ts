2f7f22df7fba8537dfc77d2e83ab6134f7c31e20
mittulmandhan <conanmittul@gmail.com>
2020-04-03 07:40:24 +0530

    process and capabilities models defined

---
+++ src/app/models/capabilities.ts
@@ -0,0 +1,8 @@
+import { Process } from './process';
+export class Capabilities {
+    processes: Process[];
+
+    constructor() {
+        this.processes = new Array();
+    }
+}
---
+++ src/app/models/process.ts
@@ -0,0 +1,5 @@
+export class Process {
+    identifier: string;
+    jobControlOptions: string;
+    outputTransmission: string;
+}
