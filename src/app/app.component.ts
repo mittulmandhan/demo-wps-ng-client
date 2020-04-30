3135fe4f21e95d6ba3948c3f65db59040aaf950f
mittulmandhan <conanmittul@gmail.com>
2020-04-30 14:50:43 +0530

    xml response converted to Capabilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ServiceinfoService } from './services/serviceinfo.service';
 import { DescribeprocessService } from './services/describeprocess.service';
+import { Capabilities } from './models/capabilities';
+import { Process } from './models/process';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,14 +18,52 @@ export class AppComponent {
   selectedUrl = 'http://geoprocessing.demo.52north.org:8080/wps/WebProcessingService';
   selectedVersion: '1.0.0' | '2.0.0' = '2.0.0';
   selectedProcessId: string;
-  response: string;
+  capabilitiesResponse: Capabilities;
 
   constructor(private serviceInfo: ServiceinfoService, private describeProcess: DescribeprocessService) {}
   call_getCapabilities() {
-    this.response = this.serviceInfo.getCapabilities_GET(this.selectedUrl, this.selectedVersion);
+    this.serviceInfo.getCapabilities_GET(this.selectedUrl, this.selectedVersion)
+    .subscribe(function(data) {
+
+      const parseString = require('xml2js').parseString;
+
+      parseString(data, (err, result: {}) => {
+        // console.log(result);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0]);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0].$.processVersion);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0].$.jobControlOptions);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0].$.outputTransmission);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0]['ows:Title'][0]);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0]['ows:Identifier'][0]);
+        const cap: Capabilities = new Capabilities();
+        cap.service = result['wps:Capabilities'].$.service;
+        cap.version = result['wps:Capabilities'].$.version;
+        cap.title = result['wps:Capabilities']['ows:ServiceIdentification'][0]['ows:Title'][0];
+        const processSummary: any[] = result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'];
+
+        processSummary.forEach((processDetails) => {
+          const process: Process = new Process();
+          // console.log(processDetails.$.processVersion);
+          // console.log(processDetails.$.jobControlOptions);
+          // console.log(processDetails.$.outputTransmission);
+          // console.log(processDetails['ows:Title'][0]);
+          // console.log(processDetails['ows:Identifier'][0]);
+          process.processVersion = processDetails.$.processVersion;
+          process.jobControlOptions = processDetails.$.jobControlOptions;
+          process.outputTransmission = processDetails.$.outputTransmission;
+          process.title = processDetails['ows:Title'][0];
+          process.identifier = processDetails['ows:Identifier'][0];
+          cap.processes.push(process);
+        });
+        this.capabilitiesResponse = cap;
+      });
+    });
   }
 
   call_DescribeProcess() {
-    this.response = this.describeProcess.describeProcess_GET(this.selectedUrl, this.selectedVersion, this.selectedProcessId);
+    this.describeProcess.describeProcess_GET(this.selectedUrl, this.selectedVersion, this.selectedProcessId)
+    .subscribe((data) => {
+      this.capabilitiesResponse = data;
+    });
   }
 }
--- src/app/services/serviceinfo.service.ts
+++ src/app/services/serviceinfo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpClientModule, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Capabilities } from '../models/capabilities';
 
 @Injectable({
   providedIn: 'root'
@@ -8,23 +9,15 @@ import { HttpClient, HttpClientModule, HttpParams, HttpHeaders } from '@angular/
 export class ServiceinfoService {
 
   xmlResponse: any;
+  capabilitiesResponse: Capabilities;
 
   constructor(private http: HttpClient) { }
 
-  xmlToCapabilities() {
-
-  }
-
-  getCapabilities_GET(serviceUrl, version): any {
+  getCapabilities_GET(serviceUrl, version): Observable<any> {
     const myParams: HttpParams = new HttpParams().set('Service', 'WPS').set('Request', 'GetCapabilities').set('Version', version);
     const myHeaders: HttpHeaders = new HttpHeaders();
 
-    this.http.get(serviceUrl, {headers: myHeaders, params: myParams, responseType: 'text'})
-    .subscribe((data) => {
-      this.xmlResponse = data;
-    });
-
-    return this.xmlResponse;
+    return this.http.get(serviceUrl, {headers: myHeaders, params: myParams, responseType: 'text'});
   }
 
 }
