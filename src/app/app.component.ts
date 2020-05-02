93f612f4c2d1833c99218843079e0677d7bf18ba
mittulmandhan <conanmittul@gmail.com>
2020-05-02 14:06:57 +0530

    capabilities response successfully display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,51 +19,17 @@ export class AppComponent {
   selectedVersion: '1.0.0' | '2.0.0' = '2.0.0';
   selectedProcessId: string;
   capabilitiesResponse: Capabilities;
+  processCount = 1;
 
   constructor(private serviceInfo: ServiceinfoService, private describeProcess: DescribeprocessService) {}
   call_getCapabilities() {
-    this.serviceInfo.getCapabilities_GET(this.selectedUrl, this.selectedVersion)
-    .subscribe(function(data) {
-
-      const parseString = require('xml2js').parseString;
-
-      parseString(data, (err, result: {}) => {
-        // console.log(result);
-        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0]);
-        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0].$.processVersion);
-        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0].$.jobControlOptions);
-        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0].$.outputTransmission);
-        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0]['ows:Title'][0]);
-        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0]['ows:Identifier'][0]);
-        const cap: Capabilities = new Capabilities();
-        cap.service = result['wps:Capabilities'].$.service;
-        cap.version = result['wps:Capabilities'].$.version;
-        cap.title = result['wps:Capabilities']['ows:ServiceIdentification'][0]['ows:Title'][0];
-        const processSummary: any[] = result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'];
-
-        processSummary.forEach((processDetails) => {
-          const process: Process = new Process();
-          // console.log(processDetails.$.processVersion);
-          // console.log(processDetails.$.jobControlOptions);
-          // console.log(processDetails.$.outputTransmission);
-          // console.log(processDetails['ows:Title'][0]);
-          // console.log(processDetails['ows:Identifier'][0]);
-          process.processVersion = processDetails.$.processVersion;
-          process.jobControlOptions = processDetails.$.jobControlOptions;
-          process.outputTransmission = processDetails.$.outputTransmission;
-          process.title = processDetails['ows:Title'][0];
-          process.identifier = processDetails['ows:Identifier'][0];
-          cap.processes.push(process);
-        });
-        this.capabilitiesResponse = cap;
-      });
-    });
+    this.capabilitiesResponse = new Capabilities();
+    this.serviceInfo.getCapabilities_GET(this.selectedUrl, this.selectedVersion, this.capabilitiesResponse);
+    console.log(this.capabilitiesResponse);
   }
 
   call_DescribeProcess() {
-    this.describeProcess.describeProcess_GET(this.selectedUrl, this.selectedVersion, this.selectedProcessId)
-    .subscribe((data) => {
-      this.capabilitiesResponse = data;
-    });
+    this.capabilitiesResponse = this.describeProcess.describeProcess_GET(this.selectedUrl, this.selectedVersion, this.selectedProcessId);
+    console.log(this.capabilitiesResponse);
   }
 }
--- src/app/services/serviceinfo.service.ts
+++ src/app/services/serviceinfo.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpClientModule, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Capabilities } from '../models/capabilities';
+import { Process } from '../models/process';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +14,50 @@ export class ServiceinfoService {
 
   constructor(private http: HttpClient) { }
 
-  getCapabilities_GET(serviceUrl, version): Observable<any> {
+  getCapabilities_GET(serviceUrl, version, cap) {
     const myParams: HttpParams = new HttpParams().set('Service', 'WPS').set('Request', 'GetCapabilities').set('Version', version);
-    const myHeaders: HttpHeaders = new HttpHeaders();
 
-    return this.http.get(serviceUrl, {headers: myHeaders, params: myParams, responseType: 'text'});
+    this.http.get(serviceUrl, {params: myParams, responseType: 'text'})
+    .subscribe((data) => {
+
+      const parseString = require('xml2js').parseString;
+
+      parseString(data, (err, result: {}) => {
+        // console.log(result);
+        console.log(result['wps:Capabilities'].$.service);
+        console.log(result['wps:Capabilities'].$.version);
+        console.log(result['wps:Capabilities']['ows:ServiceIdentification'][0]['ows:Title'][0]);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0]);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0].$.processVersion);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0].$.jobControlOptions);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0].$.outputTransmission);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0]['ows:Title'][0]);
+        // console.log(result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'][0]['ows:Identifier'][0]);
+        // const cap: Capabilities = new Capabilities();
+        // obj.capabilitiesResponse = new Capabilities();
+        cap.service = result['wps:Capabilities'].$.service;
+        cap.version = result['wps:Capabilities'].$.version;
+        cap.title = result['wps:Capabilities']['ows:ServiceIdentification'][0]['ows:Title'][0];
+        const processSummary: any[] = result['wps:Capabilities']['wps:Contents'][0]['wps:ProcessSummary'];
+
+        processSummary.forEach((processDetails) => {
+          const process: Process = new Process();
+          // console.log(processDetails.$.processVersion);
+          // console.log(processDetails.$.jobControlOptions);
+          // console.log(processDetails.$.outputTransmission);
+          // console.log(processDetails['ows:Title'][0]);
+          // console.log(processDetails['ows:Identifier'][0]);
+          process.processVersion = processDetails.$.processVersion;
+          process.jobControlOptions = processDetails.$.jobControlOptions;
+          process.outputTransmission = processDetails.$.outputTransmission;
+          process.title = processDetails['ows:Title'][0];
+          process.identifier = processDetails['ows:Identifier'][0];
+          cap.processes.push(process);
+        });
+
+      });
+      console.log(cap);
+    });
   }
 
 }
